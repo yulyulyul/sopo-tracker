492a5abe653064c2edc4e0218f346671334ef548
jieyul <legiyl94@gmail.com>
2022-04-01 12:58:47 +0900

    add | 합동 택배, 택배 조회시 송장번호가 없는 택배인 경우 처리.

--- src/main/kotlin/team/sopo/infrastructure/carrierselector/hdexp/HdResponse.kt
+++ src/main/kotlin/team/sopo/infrastructure/carrierselector/hdexp/HdResponse.kt
@@ -1,7 +1,12 @@
 package team.sopo.infrastructure.carrierselector.hdexp
 
-data class HdResponse(
+class HdResponse(
     val result: String,
     val items: List<Item>,
     val info: Info
-)
+) {
+    companion object {
+        const val SUCCESS = "suc"
+        const val FAIL = "fail"
+    }
+}
