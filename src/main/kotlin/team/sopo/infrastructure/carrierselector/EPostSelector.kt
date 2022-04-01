eece5df6438de66fcd50d2e71f1329840882b7a0
jieyul <legiyl94@gmail.com>
2022-04-01 15:49:03 +0900

    add | 우체국 택배, 택배 조회시 송장번호가 없는 택배인 경우 처리.

--- src/main/kotlin/team/sopo/infrastructure/carrierselector/EPostSelector.kt
+++ src/main/kotlin/team/sopo/infrastructure/carrierselector/EPostSelector.kt
@@ -2,13 +2,16 @@ package team.sopo.infrastructure.carrierselector
 
 import org.apache.commons.lang3.StringUtils
 import org.jsoup.Jsoup
-import org.jsoup.select.Elements
+import org.jsoup.nodes.Document
 import org.springframework.stereotype.Component
 import team.sopo.common.SupportCarrier
+import team.sopo.common.exception.ParcelNotFoundException
+import team.sopo.common.exception.ValidationException
 import team.sopo.common.parcel.*
 import team.sopo.common.util.ParcelUtil
 import team.sopo.domain.tracker.CarrierSelector
 import team.sopo.domain.tracker.TrackerCommand
+import java.util.regex.Pattern
 
 @Component
 class EpostSelector : CarrierSelector() {
@@ -18,13 +21,16 @@ class EpostSelector : CarrierSelector() {
     }
 
     override fun tracking(command: TrackerCommand.Tracking): Parcel {
+        verifyWaybillNum(command.waybillNum)
+
         val document = Jsoup.connect("https://service.epost.go.kr/trace.RetrieveDomRigiTraceList.comm")
             .ignoreContentType(true)
             .data("sid1", command.waybillNum)
             .post()
-        val epostProgress = document.select("tbody > tr")
 
-        return toParcel(epostProgress, command.carrierCode)
+        checkConvertable(document)
+
+        return toParcel(document, command.carrierCode)
     }
 
     override fun calculateStatus(criteria: String): Status {
@@ -43,7 +49,9 @@ class EpostSelector : CarrierSelector() {
         }
     }
 
-    private fun toParcel(element: Elements, carrierCode: String): Parcel {
+    private fun toParcel(document: Document, carrierCode: String): Parcel {
+
+        val element = document.select("tbody > tr")
         var parcel = Parcel(carrier = SupportCarrier.toCarrier(carrierCode))
         for (i in 0 until element.size) {
             val elements = element[i].select("tr > td")
@@ -66,4 +74,21 @@ class EpostSelector : CarrierSelector() {
 
         return parcel
     }
+
+    private fun verifyWaybillNum(waybillNum: String) {
+        val pattern = Pattern.compile("^[0-9]*?")
+        val isValidNum = waybillNum.length == 13 && pattern.matcher(waybillNum).matches()
+
+        if (!isValidNum) {
+            throw ValidationException("송장번호의 유효성을 확인해주세요. - ($waybillNum)")
+        }
+    }
+
+    private fun checkConvertable(document: Document) {
+        val progresses = document.select("tbody > tr")
+        if (progresses.size == 2) {
+            throw ParcelNotFoundException("해당 송장번호에 부합하는 택배를 찾을 수 없습니다.")
+        }
+    }
+
 }
