b4365a5ae3d081e217daf0d9adfa38e17cc866d9
jieyul <legiyl94@gmail.com>
2022-04-01 16:11:48 +0900

    add | 대신 택배, 택배 조회시 송장번호가 없는 택배인 경우 처리.

--- src/main/kotlin/team/sopo/infrastructure/carrierselector/DaesinSelector.kt
+++ src/main/kotlin/team/sopo/infrastructure/carrierselector/DaesinSelector.kt
@@ -5,10 +5,13 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.springframework.stereotype.Component
 import team.sopo.common.SupportCarrier
+import team.sopo.common.exception.ParcelNotFoundException
+import team.sopo.common.exception.ValidationException
 import team.sopo.common.parcel.*
 import team.sopo.common.util.ParcelUtil
 import team.sopo.domain.tracker.CarrierSelector
 import team.sopo.domain.tracker.TrackerCommand
+import java.util.regex.Pattern
 import kotlin.streams.toList
 
 @Component
@@ -19,10 +22,14 @@ class DaesinSelector : CarrierSelector() {
     }
 
     override fun tracking(command: TrackerCommand.Tracking): Parcel {
+        verifyWaybillNum(command.waybillNum)
+
         val document = Jsoup.connect("https://www.ds3211.co.kr/freight/internalFreightSearch.ht")
             .ignoreContentType(true)
             .data("billno", command.waybillNum)
             .get()
+
+        checkConvertable(document)
 
         return toParcel(document, command.carrierCode)
     }
@@ -63,4 +70,22 @@ class DaesinSelector : CarrierSelector() {
 
         return parcel
     }
+
+    private fun verifyWaybillNum(waybillNum: String) {
+        val num = waybillNum.replace("-", "")
+        val pattern = Pattern.compile("^[0-9]*?")
+        val isValidNum = num.length in 12..13 && pattern.matcher(num).matches()
+
+        if (!isValidNum) {
+            throw ValidationException("송장번호의 유효성을 확인해주세요. - ($waybillNum)")
+        }
+    }
+
+    private fun checkConvertable(document: Document) {
+        val progresses = document.select("table > tbody")
+        if (progresses.size == 0) {
+            throw ParcelNotFoundException("해당 송장번호에 부합하는 택배를 찾을 수 없습니다.")
+        }
+    }
+
 }
