9390da85fbc09f5d6d7205d2b975c26e6d0b8988
jieyul <legiyl94@gmail.com>
2022-04-01 12:08:38 +0900

    add | 한진 택배, 택배 조회시 송장번호가 없는 택배인 경우 처리.

--- src/main/kotlin/team/sopo/infrastructure/carrierselector/HanjinSelector.kt
+++ src/main/kotlin/team/sopo/infrastructure/carrierselector/HanjinSelector.kt
@@ -5,10 +5,13 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.springframework.stereotype.Component
 import team.sopo.common.SupportCarrier
+import team.sopo.common.exception.ParcelNotFoundException
+import team.sopo.common.exception.ValidationException
 import team.sopo.common.parcel.*
 import team.sopo.common.util.ParcelUtil
 import team.sopo.domain.tracker.CarrierSelector
 import team.sopo.domain.tracker.TrackerCommand
+import java.util.regex.Pattern
 import kotlin.streams.toList
 
 @Component
@@ -19,17 +22,21 @@ class HanjinSelector : CarrierSelector() {
     }
 
     override fun tracking(command: TrackerCommand.Tracking): Parcel {
+        verifyWaybillNum(command.waybillNum)
+
         val document = Jsoup.connect("http://www.hanjinexpress.hanjin.net/customer/hddcw18.tracking")
             .ignoreContentType(true)
             .data("w_num", command.waybillNum)
             .get()
+
+        checkConvertable(document)
 
         return toParcel(document, command.carrierCode)
     }
 
     override fun calculateStatus(criteria: String): Status {
         if (StringUtils.isBlank(criteria)) {
-            throw IllegalStateException("Status를 처리할 수 없습니다.(EPost)")
+            throw IllegalStateException("Status를 처리할 수 없습니다.(Hanjin)")
         }
         val processedCriteria = criteria.trim().replace(" ", "")
         return if (processedCriteria.contains("집하") || processedCriteria.contains("접수")) {
@@ -68,4 +75,21 @@ class HanjinSelector : CarrierSelector() {
 
         return parcel
     }
+
+    private fun verifyWaybillNum(waybillNum: String) {
+        val pattern = Pattern.compile("^[0-9]*?")
+        val isValidNum = waybillNum.length in 12..14 && pattern.matcher(waybillNum).matches()
+
+        if (!isValidNum) {
+            throw ValidationException("송장번호의 유효성을 확인해주세요. - ($waybillNum)")
+        }
+    }
+
+    private fun checkConvertable(document: Document) {
+        val progresses = document.select("table[class='board-list-table'] tbody > tr")
+        if (progresses.size == 0) {
+            throw ParcelNotFoundException("해당 송장번호에 부합하는 택배를 찾을 수 없습니다.")
+        }
+    }
+
 }
