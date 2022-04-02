5119241b4e1a22e7acbb50984f16aa8d11da1e28
jieyul <legiyl94@gmail.com>
2022-04-03 00:39:13 +0900

    fix | 천일택배, 시간 포맷 정리

--- src/main/kotlin/team/sopo/infrastructure/carrierselector/ChunilpsSelector.kt
+++ src/main/kotlin/team/sopo/infrastructure/carrierselector/ChunilpsSelector.kt
@@ -3,6 +3,8 @@ package team.sopo.infrastructure.carrierselector
 import org.apache.commons.lang3.StringUtils
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
 import org.springframework.stereotype.Component
 import team.sopo.common.SupportCarrier
 import team.sopo.common.exception.ParcelNotFoundException
@@ -10,13 +12,19 @@ import team.sopo.common.exception.ValidationException
 import team.sopo.common.parcel.*
 import team.sopo.common.util.ParcelUtil.Companion.determineState
 import team.sopo.common.util.ParcelUtil.Companion.sorting
+import team.sopo.common.util.TimeUtil
 import team.sopo.domain.tracker.CarrierSelector
 import team.sopo.domain.tracker.TrackerCommand
+import java.time.format.DateTimeFormatter
 import java.util.regex.Pattern
 import kotlin.streams.toList
 
 @Component
 class ChunilpsSelector : CarrierSelector() {
+
+    companion object {
+        private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+    }
 
     override fun support(carrierCode: String): Boolean {
         return StringUtils.equals(carrierCode, SupportCarrier.CHUNILPS.code)
@@ -25,7 +33,7 @@ class ChunilpsSelector : CarrierSelector() {
     override fun tracking(command: TrackerCommand.Tracking): Parcel {
         verifyWaybillNum(command.waybillNum)
 
-        val document = Jsoup.connect("http://www.chunil.co.kr/HTrace/HTrace.jsp")
+        val document = Jsoup.connect("https://www.chunil.co.kr/HTrace/HTrace.jsp")
             .ignoreContentType(true)
             .data("transNo", command.waybillNum)
             .get()
@@ -55,28 +63,42 @@ class ChunilpsSelector : CarrierSelector() {
         var parcel = Parcel(carrier = SupportCarrier.toCarrier(carrierCode))
         val elements = document.select("table[cellspacing='1']")
 
-        parcel.from = From(elements[0].select("tbody > tr > td")[1].text())
-        parcel.to = To(elements[1].select("tbody > tr > td")[1].text())
+        parcel.from = toFrom(elements)
+        parcel.to = toTo(elements)
         parcel.item = elements[2].select("tbody > tr > td")[1].text()
-
-        val progress = elements[4].select("tbody > tr")
-        parcel.progresses.addAll(
-            progress.stream()
-                .filter { it != progress.first() }
-                .map {
-                    val detail = it.select("td")
-                    Progresses(
-                        time = detail[0].text(),
-                        location = Location(detail[1].text()),
-                        status = calculateStatus(detail[3].text()),
-                        description = detail[3].text()
-                    )
-                }.toList()
-        )
+        parcel.progresses.addAll(toProgresses(elements = elements[4].select("tbody > tr")))
         parcel = sorting(parcel)
         parcel.state = determineState(parcel)
 
         return parcel
+    }
+
+    private fun toFrom(elements: Elements): From {
+        return From(elements[0].select("tbody > tr > td")[1].text())
+    }
+
+    private fun toTo(elements: Elements): To {
+        return To(elements[1].select("tbody > tr > td")[1].text())
+    }
+
+    private fun toProgresses(elements: Elements): List<Progresses> {
+        return elements.stream()
+            .filter { it != elements.first() }
+            .filter { checkTimeFormat(it) }
+            .map {
+                val detail = it.select("td")
+                Progresses(
+                    time = TimeUtil.convert("${detail[0].text()} 00:00", formatter),
+                    location = Location(detail[1].text()),
+                    status = calculateStatus(detail[3].text()),
+                    description = detail[3].text()
+                )
+            }.toList()
+    }
+
+    private fun checkTimeFormat(element: Element): Boolean {
+        val data = element.select("td")
+        return TimeUtil.checkTimeFormat("${data[0].text()} 00:00", formatter)
     }
 
     private fun verifyWaybillNum(waybillNum: String) {
